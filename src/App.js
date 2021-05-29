9d55c3c4ad2d0dfb6bce22786c58c81185f4f095
Rohan Nagar <rohan17nagar@gmail.com>
2021-05-29 19:53:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { TicketBook } from "./TicketBook";
+
+function App() {
+  return (
+    <div>
+      <TicketBook />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/TicketBook.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+export const TicketBook = () => {
+  function handleEvent(e, dataValues) {}
+  return (
+    <div>
+      <div>Hello</div>
+    </div>
+  );
+};
